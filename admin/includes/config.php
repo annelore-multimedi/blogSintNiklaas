d654855a1e83248c9b1381df99f48055ca2b8a2e
annelore-multimedi <annelore.maes@multimedi.be>
2020-05-18 09:17:58 +0200

    tem parent class

---
+++ admin/includes/config.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_HOST','localhost:3308');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'dbblog');
+?>
+
+<?php
+/*define('DB_HOST','localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'blog');
+*/?>
